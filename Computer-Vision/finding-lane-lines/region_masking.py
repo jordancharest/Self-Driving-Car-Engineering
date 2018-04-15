55b1ec99cd24c783f398add4ccbd2ea72dd28805
Jordan <jordancharest3@gmail.com>
2018-04-14 21:56:27 -0400

    Finished color selection

---
+++ Computer-Vision/finding-lane-lines/region_masking.py
@@ -0,0 +1,50 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+
+# Read in the image
+image = mpimg.imread('road.jpg')
+
+# Grab the x and y sizes and make two copies of the image
+# With one copy we'll extract only the pixels that meet the selection,
+# then we'll paint those pixels red in the original image to see our selection 
+# overlaid on the original.
+ysize = image.shape[0]
+xsize = image.shape[1]
+color_select= np.copy(image)
+line_image = np.copy(image)
+
+# Define the color criteria
+red_threshold = 200
+green_threshold = 200
+blue_threshold = 200
+rgb_threshold = [red_threshold, green_threshold, blue_threshold]
+
+# Define a triangle region of interest
+# At the bottom, take the middle 80% of the image, move the apex 60% down the image
+left_bottom = [int(0.1*xsize), ysize]
+right_bottom = [int(0.9*xsize), ysize]
+apex = [xsize//2, int(0.6*ysize)]
+
+fit_left = np.polyfit((left_bottom[0], apex[0]), (left_bottom[1], apex[1]), 1)
+fit_right = np.polyfit((right_bottom[0], apex[0]), (right_bottom[1], apex[1]), 1)
+fit_bottom = np.polyfit((left_bottom[0], right_bottom[0]), (left_bottom[1], right_bottom[1]), 1)
+
+# Mask pixels below the threshold
+color_thresholds = (image[:,:,0] < rgb_threshold[0]) | \
+                    (image[:,:,1] < rgb_threshold[1]) | \
+                    (image[:,:,2] < rgb_threshold[2])
+
+# Find the region inside the lines
+XX, YY = np.meshgrid(np.arange(0, xsize), np.arange(0, ysize))
+region_thresholds = (YY > (XX*fit_left[0] + fit_left[1])) & \
+                    (YY > (XX*fit_right[0] + fit_right[1])) & \
+                    (YY < (XX*fit_bottom[0] + fit_bottom[1]))
+# Mask color selection
+color_select[color_thresholds] = [0,0,0]
+# Find where image is both colored right and in the region
+line_image[~color_thresholds & region_thresholds] = [255,0,0]
+
+# Display our two output images
+#plt.imshow(color_select)
+plt.imshow(line_image)
