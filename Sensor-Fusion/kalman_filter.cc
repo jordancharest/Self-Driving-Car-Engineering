10cf74337366fe0208191f51e6656ed77e16b632
Jordan Charest <jordancharest3@gmail.com>
2018-10-27 19:35:07 -0400

    Remove useless temporaries

--- Sensor-Fusion/kalman_filter.cc
+++ Sensor-Fusion/kalman_filter.cc
@@ -8,20 +8,18 @@ KalmanFilter::~KalmanFilter() {
 
 void KalmanFilter::Predict() {
     x_ = F_ * x_;
-    MatrixXd Ft = F_.transpose();
-    P_ = F_ * P_ * Ft + Q_;
+    P_ = F_ * P_ * F_.transpose() + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
-    VectorXd z_pred = H_ * x_;
-    VectorXd y = z - z_pred;
-    MatrixXd Ht = H_.transpose();
-    MatrixXd S = H_ * P_ * Ht + R_;
-    MatrixXd Si = S.inverse();
-    MatrixXd PHt = P_ * Ht;
-    MatrixXd K = PHt * Si;
+    Eigen::MatrixXd Ht = H.transpose();
 
-    //new estimate
+    Eigen::VectorXd z_pred = H_ * x_;
+    Eigen::VectorXd y = z - z_pred;
+    Eigen::MatrixXd S = H_ * P_ * Ht + R_;
+    Eigen::MatrixXd K = P * Ht * S.inverse();
+
+    // new estimate
     x_ = x_ + (K * y);
     long x_size = x_.size();
     MatrixXd I = MatrixXd::Identity(x_size, x_size);
