d6e6f0d4016f2be1fb5815be784229775e037532
Jordan Charest <jordancharest3@gmail.com>
2018-10-27 19:35:34 -0400

    Added measurement and prediction updates

--- Sensor-Fusion/tracking.cc
+++ Sensor-Fusion/tracking.cc
@@ -66,14 +66,24 @@ void Tracking::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
     float dt = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000.0; //dt - expressed in seconds
     previous_timestamp_ = measurement_pack.timestamp_;
     
-    // TODO: YOUR CODE HERE
-    //1. Modify the F matrix so that the time is integrated
-    //2. Set the process covariance matrix Q
-    //3. Call the Kalman Filter predict() function
-    //4. Call the Kalman Filter update() function
-    // with the most recent raw measurements_
+    // Modify the F matrix so that the time step is integrated
+    kf_.F_(0,2) = dt;
+    kf_.F_(1,3) = dt;
+
+    // Set the process covariance matrix Q
+    kf_.Q_ = MatrixXd(4, 4);
+    kf_.Q_ << pow(dt, 4) / 4 * noise_ax,  0,  pow(dt, 3) / 2 * noise_ax,  0,
+              0,  pow(dt, 4) / 4 * noise_ay,  0,  pow(dt, 3) / 2 * noise_ay,
+              pow(dt, 3) / 2 * noise_ax,  0,  pow(dt, 2) * noise_ax,  0,
+              0,  pow(dt, 3) / 2 * noise_ay,  0,  pow(dt, 2) * noise_ay;
+
+    // predict update
+    kf_.Predict();
+
+    // measurement update with the most recent raw measurements
+    kf_.Update(measurement_pack.raw_measurements_);
+
     
     std::cout << "x_= " << kf_.x_ << std::endl;
     std::cout << "P_= " << kf_.P_ << std::endl;
-
 }
